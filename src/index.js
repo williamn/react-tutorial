69920425c3d4a2b21de800ea053e830c942f1ce8
William Notowidagdo <wnotowidagdo@gmail.com>
2016-08-18 15:30:51 +0700

    hook up the data model

--- src/index.js
+++ src/index.js
@@ -3,7 +3,12 @@ import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox';
 import './index.css';
 
+var data = [
+  {id: 1, author: "Pete Hunt", text: "This is one comment"},
+  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
+];
+
 ReactDOM.render(
-  <CommentBox />,
+  <CommentBox data={data} />,
   document.getElementById('content')
 );

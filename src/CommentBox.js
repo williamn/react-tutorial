0515cf62fc7547679a0ec5a096ccf99bdc99b66c
William Notowidagdo <wnotowidagdo@gmail.com>
2016-08-18 16:23:25 +0700

    add new comments

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -23,6 +23,21 @@ class CommentBox extends Component {
     });
   }
 
+  handleCommentSubmit(comment) {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      type: 'POST',
+      data: comment,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  }
+
   componentDidMount() {
     this.loadCommentsFromServer();
     setInterval(this.loadCommentsFromServer, this.props.pollInterval);
@@ -33,7 +48,7 @@ class CommentBox extends Component {
       <div className="commentBox">
         <h1>Comments</h1>
         <CommentList data={this.state.data} />
-        <CommentForm />
+        <CommentForm onCommentSubmit={this.handleCommentSubmit} />
       </div>
     );
   }
--- src/CommentForm.js
+++ src/CommentForm.js
@@ -1,10 +1,48 @@
 import React, { Component } from 'react';
 
 class CommentForm extends Component {
+  getInitialState() {
+    return {author: '', text: ''};
+  }
+
+  handleAuthorChange(e) {
+    this.setState({author: e.target.value});
+  }
+
+  handleTextChange(e) {
+    this.setState({text: e.target.value});
+  }
+
+  handleSubmit(e) {
+    e.preventDefault();
+    var author = this.state.author.trim();
+    var text = this.state.text.trim();
+    if (!text || !author) {
+      return;
+    }
+
+    this.props.onCommentSubmit({author: author, text: text});
+    this.setState({author: '', text: ''});
+  }
+
   render() {
     return (
       <div className="commentForm">
-        Hello, world! I am a CommentForm.
+        <form className="commentForm" onSubmit={this.handleSubmit}>
+          <input 
+           type="text" 
+           placeholder="Your name"
+           value={this.state.author}
+           onChange={this.handleAuthorChange} 
+          />
+          <input
+           type="text" 
+           placeholder="Say something ..."
+           value={this.state.text}
+           onChange={this.handleTextChange} 
+          />
+          <input type="submit" value="Post" />
+        </form>
       </div>
     );
   }

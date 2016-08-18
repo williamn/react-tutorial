5f4fdbddb5da90f73cae5525dbbfa721c93180e9
William Notowidagdo <wnotowidagdo@gmail.com>
2016-08-18 14:58:31 +0700

    add CommentBox component
    remove App component

--- src/index.js
+++ src/index.js
@@ -1,9 +1,9 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import App from './App';
+import CommentBox from './CommentBox';
 import './index.css';
 
 ReactDOM.render(
-  <App />,
-  document.getElementById('root')
+  <CommentBox />,
+  document.getElementById('content')
 );

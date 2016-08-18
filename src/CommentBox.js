16da7d744c1617b7d7fadcd3ccf53a7e5b170fdb
William Notowidagdo <wnotowidagdo@gmail.com>
2016-08-18 15:42:57 +0700

    reactive state

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -3,11 +3,15 @@ import CommentList from './CommentList';
 import CommentForm from './CommentForm';
 
 class CommentBox extends Component {
+  getInitialState() {
+    return {data: []};
+  }
+
   render() {
     return (
       <div className="commentBox">
         <h1>Comments</h1>
-        <CommentList data={this.props.data} />
+        <CommentList data={this.state.data} />
         <CommentForm />
       </div>
     );

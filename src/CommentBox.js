8dbdb467b668ee29b2afd6b18dc94a503e76e6ed
William Notowidagdo <wnotowidagdo@gmail.com>
2016-08-18 16:04:51 +0700

    updating state

--- src/CommentBox.js
+++ src/CommentBox.js
@@ -2,9 +2,30 @@ import React, { Component } from 'react';
 import CommentList from './CommentList';
 import CommentForm from './CommentForm';
 
+var $ = require('jquery');
+
 class CommentBox extends Component {
   getInitialState() {
     return {data: []};
+  }
+
+  loadCommentsFromServer() {
+    $.ajax({
+      url: this.props.url,
+      dataType: 'json',
+      cache: false,
+      success: function(data) {
+        this.setState({data: data});
+      }.bind(this),
+      error: function(xhr, status, err) {
+        console.error(this.props.url, status, err.toString());
+      }.bind(this)
+    });
+  }
+
+  componentDidMount() {
+    this.loadCommentsFromServer();
+    setInterval(this.loadCommentsFromServer, this.props.pollInterval);
   }
 
   render() {
--- src/index.js
+++ src/index.js
@@ -3,12 +3,7 @@ import ReactDOM from 'react-dom';
 import CommentBox from './CommentBox';
 import './index.css';
 
-var data = [
-  {id: 1, author: "Pete Hunt", text: "This is one comment"},
-  {id: 2, author: "Jordan Walke", text: "This is *another* comment"}
-];
-
 ReactDOM.render(
-  <CommentBox url="http://localhost:3001/api/comments" />,
+  <CommentBox url="http://localhost:3001/api/comments" pollInterval={2000} />,
   document.getElementById('content')
 );

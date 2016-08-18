430095277fae42fdf02b23bd5bcab6a6554fa8d7
William Notowidagdo <wnotowidagdo@gmail.com>
2016-08-18 15:17:32 +0700

    add Markdown to format text inline

--- src/Comment.js
+++ src/Comment.js
@@ -1,13 +1,19 @@
 import React, { Component } from 'react';
 
 class Comment extends Component {
+	rawMarkup() {
+		var md = new Remarkable();
+		var rawMarkup = md.render(this.props.children.toString());
+		return { __html: rawMarkup };
+	}
+
   render() {
     return (
       <div className="comment">
         <h2 className="commentAuthor">
           {this.props.author}
         </h2>
-        {this.props.children}
+        <span dangerouslySetInnerHTML={this.rawMarkup()} />
       </div>
     );
   }
